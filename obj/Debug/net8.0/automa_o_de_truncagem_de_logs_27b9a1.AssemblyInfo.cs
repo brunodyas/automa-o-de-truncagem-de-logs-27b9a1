1c8d5d36f1758ba4cb91e3750a0129882b823024
BrunoDyas <brunodyas@users.noreply.github.com>
2026-04-27 14:25:35 -0300

    feat: configuração estática
    intent: feature

--- obj/Debug/net8.0/automa_o_de_truncagem_de_logs_27b9a1.AssemblyInfo.cs
+++ obj/Debug/net8.0/automa_o_de_truncagem_de_logs_27b9a1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("automa_o_de_truncagem_de_logs_27b9a1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e539846fa3d9e99d8da567b40335b556020ecf4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+242f41ee3d020ca8fcb0dce56e9a9c0dde679e4f")]
 [assembly: System.Reflection.AssemblyProductAttribute("automa_o_de_truncagem_de_logs_27b9a1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("automa_o_de_truncagem_de_logs_27b9a1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
